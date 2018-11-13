f0d813198ba509bc4fd15cd7da00187aff228882
amazingfly <derekstich@gmail.com>
2018-11-13 02:48:14 +0000

    calculizing

--- rgb.go
+++ rgb.go
@@ -1,19 +1,23 @@
 package main
 
 import (
+	"fmt"
+	"strconv"
 	"sync"
 	"time"
 
 	rpio "github.com/go-rpio"
 )
 
+//
+//dutyCycle = tOn / hz
+//
+
 var wg = new(sync.WaitGroup)
 
 func main() {
-	var flashTime = 300
-	var pwmHz = 55 * time.Nanosecond
+	var pwmHz = 60
 	var dutyCycle = 100
-
 	rpio.Open()
 	defer rpio.Close()
 
@@ -24,21 +28,34 @@ func main() {
 	greenPin := rpio.Pin(4)
 	greenPin.Output()
 
+	
+/*
 	for {
+		wg.Add(1)
 		go pinOn(pwmHz, dutyCycle, &redPin)
+		wg.Add(1)
 		go pinOn(pwmHz, dutyCycle, &bluePin)
+		wg.Add(1)
 		go pinOn(pwmHz, dutyCycle, &greenPin)
 		wg.Wait()
+		//dutyCycle--
+		if dutyCycle < 0{
+			dutyCycle = 100
+		}
 	}
+	*/
 }
 
-func pinOn(pwmHz, dutyCycle int, pin rpio.Pin) bool {
-	wg.Add()
-	var hz = pwmHz * time.Millisecond
-	var pulse = ((dutyCycle / 10) * pwmHz) * time.Millisecond
+func pinOn(pwmHz, dutyCycle int, pin *rpio.Pin) {
+	//wg.Add(1)
+	var pString = strconv.Itoa((dutyCycle  / 100 ) * pwmHz)
+	pulse, err := time.ParseDuration((pString+"ms"))
+	if err != nil{
+		fmt.Println(err)
+	}
 	pin.High()
 	time.Sleep(pulse)
 	pin.Low()
-	time.Sleep(pulse)
+	time.Sleep(pulse )
 	wg.Done()
 }
