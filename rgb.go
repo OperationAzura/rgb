c01648d7068c1fe359cf5c7d62bb3ed9b1d6e8e6
amazingfly <derekstich@gmail.com>
2018-11-12 19:58:55 -0500

    go-rpio

---
+++ rgb.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"time"
+
+	rpio "github.com/go-rpio"
+)
+
+func main() {
+	var t = 1
+	var flashTime = 300
+	var pwmHz = 55
+
+	rpio.Open()
+	defer rpio.Close()
+
+	bluePin := rpio.Pin(2)
+	bluePin.Output()
+	redPin := rpio.Pin(3)
+	redPin.Output()
+	greenPin := rpio.Pin(4)
+	greenPin.Output()
+
+	for {
+		redPin.Low()
+		bluePin.Low()
+		greenPin.Low()
+		time.Sleep(pwmHz)
+		redPin.High()
+		bluePin.High()
+		greenPin.High()
+		time.Sleep(pwmHz)
+	}
+
+}
